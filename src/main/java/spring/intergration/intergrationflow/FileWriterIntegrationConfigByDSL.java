05e3258e373825477f46983c3fc5fc88132137d1
aladindev <kjune0205@naver.com>
2022-10-22 15:19:17 +0900

    Spring integration flow by DSL

---
+++ src/main/java/spring/intergration/intergrationflow/FileWriterIntegrationConfigByDSL.java
@@ -0,0 +1,36 @@
+package spring.intergration.intergrationflow;
+
+import java.io.File;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
+import org.springframework.integration.file.support.FileExistsMode;
+
+@Configuration
+public class FileWriterIntegrationConfigByDSL {
+	
+	/*
+	 * 자바 DSL : Domain Specific Language 구성 방법
+	 * 통합 플로우의 각 컴포넌트를 별도의 빈으로 선언하지 않고 
+	 * 전체 플로우를 하나의 빈으로 선언하는 방식.
+	 * */
+	@Bean
+	public IntegrationFlow filewWriterFlow() {
+		
+		return IntegrationFlows
+				.from(MessageChannels.direct("textInChannel")) //인바운드 채널
+				.<String, String>transform(t -> t.toUpperCase()) //변환기를 선언한다.
+				//변환기를 아웃바운드 채널 어댑터와 연결하는 채널을 명시한다.
+				.channel(MessageChannels.direct("fileWriterChannel")) 
+				.handle(Files //파일에 쓰는 것을 처리한다.
+						.outboundAdapter(new File("/tmp/sia5/files"))
+						.fileExistsMode(FileExistsMode.APPEND)
+						.appendNewLine(true))
+				.get();
+	}
+
+}
