8cac62c75ea084443e599d808ada541255c24624
aladindev <kjune0205@naver.com>
2022-10-22 15:44:33 +0900

    QueueChannel

--- src/main/java/spring/intergration/intergrationflow/FileWriterIntegrationConfig.java
+++ src/main/java/spring/intergration/intergrationflow/FileWriterIntegrationConfig.java
@@ -4,14 +4,23 @@ import java.io.File;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.support.FileExistsMode;
 import org.springframework.integration.transformer.GenericTransformer;
+import org.springframework.messaging.MessageChannel;
 
 @Configuration
 public class FileWriterIntegrationConfig {
+	
+	// 별도의 채널 지정
+	@Bean
+	public MessageChannel orderChannel() {
+		return new QueueChannel();
+	}
 	
 	@Bean
 	@Transformer(inputChannel="textInChannel"  //변환기 빈을 선언한다.
@@ -25,7 +34,9 @@ public class FileWriterIntegrationConfig {
 	 * @ServiceActivator : fileWriterChannel로부터 메세지를 받아서 
 	 * FileWritingMessageHandler의 인스턴스로 정의된 서비스에 넘겨줌을 나타낸다.
 	 * */
-	@ServiceActivator(inputChannel="fileWriterChannel") 
+	
+	//QueueChannel을 사용할 시에는 폴링 구성(도착한 메세지가 있는지 지속적으로 확인함)이 필요하다. ms
+	@ServiceActivator(inputChannel="orderChannel", poller=@Poller(fixedRate="1000")) 
 	public FileWritingMessageHandler fileWriter() { // 파일-쓰기 빈을 선언한다.
 		FileWritingMessageHandler handler =
 					new FileWritingMessageHandler(new File("/tmp/sia5/files"));
