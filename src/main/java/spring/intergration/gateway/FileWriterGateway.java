77b2aae9dedde6b80f21d61f28d778db6f570c28
aladindev <kjune0205@naver.com>
2022-10-22 14:34:59 +0900

    Spring intergration

--- src/main/java/spring/intergration/gateway/FileWriterGateway.java
+++ src/main/java/spring/intergration/gateway/FileWriterGateway.java
@@ -4,7 +4,11 @@ import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.file.FileHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 
-//메세지 게이트를 선언하는 어노테이션 
+/**
+ * 메세지 게이트를 선언하는 어노테이션 
+ * FileWriterGateway 인터페이스의 구현체(클래스)를 런타임 시에 생성하라고 
+ * 스프링 통합에 알려준다.
+ */
 @MessagingGateway(defaultRequestChannel="textInChannel")
 public interface FileWriterGateway {
 	
